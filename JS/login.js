1dd3d74cf941713cd9d372fa4a21dfc8804235bf
MrTigregg <128178649+MrTigregg@users.noreply.github.com>
2023-03-29 09:20:10 -0400

    Update login.js

--- JS/login.js
+++ JS/login.js
@@ -1,26 +1,18 @@
 const loginForm = document.querySelector('.login-form');
-
-// Add an event listener for form submission
 loginForm.addEventListener('submit', function (e) {
-    e.preventDefault(); // prevent the form from submitting
-
-    // Get the input values from the form
+    e.preventDefault(); 
     const username = loginForm.querySelector('#username').value;
     const password = loginForm.querySelector('#password').value;
 
-    // Send a request to fetch the users from the JSON file
     fetch('users.json')
         .then(response => response.json())
         .then(users => {
-            // Check if the user exists in the users array
             const user = users.find(user => user.username === username && user.password === password);
 
             if (user) {
-                // If the user exists, log them in and redirect to form.html
                 console.log('Login successful!');
-                window.location.href = 'form.html'; // set the URL of the destination page
+                window.location.href = 'form.html';
             } else {
-                // If the user does not exist, show an error message
                 console.log('Invalid username or password');
             }
         })
