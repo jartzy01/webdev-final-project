b227bd6a47344d7437617ecd4cbdb9a43081a8a6
Mr. Tigregg <granadosgiraldo@gmail.com>
2023-03-29 11:04:21 -0400

    update

--- JS/register.js
+++ JS/register.js
@@ -1,20 +1,18 @@
-const form = document.getElementById('user-form');
+const registerForm = document.getElementById('register-form');
 
-form.addEventListener('submit', (event) => {
+registerForm.addEventListener('submit', (event) => {
     event.preventDefault();
-    const username = form.elements.username.value;
-    const email = form.elements.email.value;
-    const password = form.elements.password.value;
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
     const user = {
-        username,
-        email,
-        password
+        name: name,
+        email: email,
+        password: password
     };
-    const users = JSON.parse(localStorage.getItem('users')) || [];
+    const users = JSON.parse(localStorage.getItem('users') || '[]');
     users.push(user);
     localStorage.setItem('users', JSON.stringify(users));
-    alert('User registered successfully!');
 });
-
 
 
