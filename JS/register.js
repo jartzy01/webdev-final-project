675d7358027d9265e1142877f06c15d3c30cd29d
Mr. Tigregg <granadosgiraldo@gmail.com>
2023-03-29 11:22:48 -0400

    update

--- JS/register.js
+++ JS/register.js
@@ -1,18 +1,29 @@
-const registerForm = document.getElementById('register-form');
-
-registerForm.addEventListener('submit', (event) => {
+const form = document.querySelector('form');
+form.addEventListener('submit', registerUser);
+async function registerUser(event) {
     event.preventDefault();
+
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
-    const user = {
-        name: name,
-        email: email,
-        password: password
-    };
-    const users = JSON.parse(localStorage.getItem('users') || '[]');
-    users.push(user);
-    localStorage.setItem('users', JSON.stringify(users));
-});
 
+    const user = { name, email, password };
+
+    try {
+        const response = await fetch('users.json', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        });
+        if (response.ok) {
+            alert('User registered successfully!');
+        } else {
+            alert('Error registering user');
+        }
+    } catch (error) {
+        console.error('Error registering user:', error);
+    }
+}
 
